2ad59789cac8d2a756acbea37ccf763a25422cfd
Sharjeelbutt410 <sharjeelbutt934@gmail.com>
2026-07-24 22:12:47 +0500

    Add files via upload

---
+++ day_14/Student Management System/main.cpp
@@ -0,0 +1,12 @@
+#include "studentmanager.h"
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    StudentManager manager;
+    manager.savefile();
+    manager.menu();
+
+    return 0;
+}
